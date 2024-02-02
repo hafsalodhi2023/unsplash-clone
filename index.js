33c356633860c555e8690c549833f693fe4be7b5
Hafsa Khan Lodhi <hafsalodhi2023@gmail.com>
2024-02-02 14:09:59 +0500

    interface and random images completed

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-let acc
+let accessToken = "WHtjYtx51173CvnaZzIZcrGqkbRuIeevQKiSfJM3wSk";
+let randomImages = `https://api.unsplash.com/photos/random?client_id=${accessToken}&count=30`;
+let gallery = document.querySelector("#gallery");
+
+fetch(randomImages)
+  .then((res) => {
+    return res.json();
+  })
+  .then((finalRes) => {
+    console.log(finalRes);
+    finalRes.forEach((data) => {
+      console.log(data);
+      gallery.appendChild(createImage(data.urls.regular));
+    });
+  })
+  .catch((error) => {
+    console.log(`Error: ${error}`);
+  });
+
+function createImage(url) {
+  let div = document.createElement("div");
+  div.innerHTML = `<img src=${url} />`;
+  return div;
+}
